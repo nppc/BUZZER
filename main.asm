f55f7968194fc78b46afbf86142530f0f0e32b0a
Pavel Palonen <pavel.palonen@gmail.com>
2017-08-23 15:02:07 +0200

    Added Mosfet version
    No HW PWM yet

--- main.asm
+++ main.asm
@@ -1,19 +1,28 @@
 ;avra main.asm
 
-;.include "tn4def.inc"
 ;.include "tn5def.inc"
 ;.include "tn9def.inc"
 .include "tn10def.inc"
 
+;#define MOSFET_VERSION_HWPWM
+#define MOSFET_VERSION_SWPWM	; comment if no mosfet PCB
+
 .EQU 	FREQ_CONST		= 742-1	; constant for frequency generator (183us for 50% duty cycle of 2739Hz). -1 for CALL/RET
 
+#ifdef MOSFET_VERSION_SWPWM
+.EQU	BUZZ_Out_ON		= 1 << PB0 	; Tone ON
+.EQU	BUZZ_Out_OFF	= 0 << PB0 	; Tone OFF
+.EQU	CONTROL_PIN		= PB1	; Control 
+#else
 .EQU	BUZZ_Out_ON		= 1 << PB0 | 1 << PB1 | 1 << PB2	; Tone ON
-.EQU	BUZZ_Out_OFF	= 0 << PB0 | 0 << PB1 | 0 << PB2	; Tone ON
-
+.EQU	BUZZ_Out_OFF	= 0 << PB0 | 0 << PB1 | 0 << PB2	; Tone OFF
 .EQU	CONTROL_PIN		= PB3	; Control (RESET pin with voltage divider)
+#endif
 
 
-.def	tmp			= r16
+
+.def	tmp		= r16
+.def	tmp1	= r17
 .def	pwm_cL	= r18
 .def	pwm_cH	= r19
 
@@ -55,3 +64,16 @@ PWM_F1:	dec  pwm_cL					; 1
 		dec  pwm_cH					; 1
 		brne PWM_F1					; 1/2
 		ret							; 4
+
+#ifdef MOSFET_VERSION_HWPWM		
+PWM_init:
+		ldi tmp, (1<<COM0A1) | (1<<WGM01) | (1<<WGM00)	; Phase correct (OCR0A), mode 11
+		out TCCR0A, tmp
+		ldi tmp, (1<<WGM03) | (0<<WGM02) | (0<<CS02) | (0<<CS01) | (1<<CS00)	; ; Phase correct (OCR0A), mode 11
+		out TCCR0B, tmp
+		ldi tmp, LOW(FREQ_CONST)
+		ldi tmp1, HIGH(FREQ_CONST)
+		out OCR0AH, tmp1
+		out OCR0AL, tmp
+		ret
+#endif
